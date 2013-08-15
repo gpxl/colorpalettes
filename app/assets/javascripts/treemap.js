e63553ae9a685d37f59edb2509115a77f106df16
gpxl <gerlando@gmail.com>
2013-08-15 14:45:19 -0700

    Added example to homepage

---
+++ app/assets/javascripts/treemap.js
@@ -0,0 +1,34 @@
+var draw_treemap = function(root, target) {
+
+  var margin = {top: 40, bottom: 10},
+      width = 960,
+      height = 500 - margin.top - margin.bottom;
+
+  var treemap = d3.layout.treemap()
+      .size([width, height])
+      .sticky(true)
+      .value(function(d) { return d.size; });
+
+  var div = d3.select(target).append("div")
+      .style("position", "relative")
+      .style("width", (width) + "px")
+      .style("height", (height + margin.top + margin.bottom) + "px")
+      .style("top", margin.top + "px")
+      .style("margin-left", "auto")
+      .style("margin-right", "auto");
+
+  var node = div.datum(root).selectAll(".node")
+      .data(treemap.nodes)
+      .enter().append("div")
+      .attr("class", "node")
+      .call(position)
+      .style("background", function(d) { return d.name; })
+      .text(function(d) { return d.name; });
+}
+
+function position() {
+  this.style("left", function(d) { return d.x + "px"; })
+      .style("top", function(d) { return d.y + "px"; })
+      .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+      .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+}
--- app/controllers/colormaps_controller.rb
+++ app/controllers/colormaps_controller.rb
@@ -7,6 +7,8 @@ class ColormapsController < ApplicationController
   def create
     @css = DryCss::CSS.new(params[:url])
     @content = @css.colors
+    @treemap = {:name => 'flare', :children => []}
+    @content[:counts].map{|k,v| @treemap[:children].push({'name' => k[-1,1] == ';' ? k[0, k.size-1] : k, 'size' => v})}
     render 'colormaps/show'
   end
 
