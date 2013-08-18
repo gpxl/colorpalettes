afe6225f70802bbed66264181a09b5d12c57eea8
gpxl <gerlando@gmail.com>
2013-08-18 12:27:03 -0700

    removed name of parent node

--- app/models/colormap.rb
+++ app/models/colormap.rb
@@ -18,7 +18,7 @@ class Colormap < ActiveRecord::Base
     @css = DryCss::CSS.new(*@site.uris)
     self.color_count = @css.colors[:counts].count
     self.reference_count = @css.colors[:total]
-    self.treemap = {:name => 'flare', :children => []}
+    self.treemap = {:name => '', :children => []}
     @css.colors[:counts].map{|k,v| self.treemap[:children].push({'name' => k[-1,1] == ';' ? k[0, k.size-1] : k, 'size' => v})}
   end
 
